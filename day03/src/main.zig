a2b90ae3451110de239e4ca3a7e2f7a3430e36a5
Sérgio Sarabando <ssarabando@gmail.com>
2023-12-20 21:17:22 +0000

    Day 3, part 2.

--- day03/src/main.zig
+++ day03/src/main.zig
@@ -29,74 +29,6 @@ const PossiblePartNumber = struct {
     }
 };
 
-fn _adjacency_tests(ppn: PossiblePartNumber, expected: Symbol, symbols: []const Symbol) !void {
-    for (symbols) |symbol| {
-        const actual = ppn.is_adjacent_to_symbol(&symbol);
-        if (meta.eql(expected, symbol)) {
-            try expect(actual);
-        } else {
-            try expect(!actual);
-        }
-    }
-}
-
-// yx0123456789
-// 0 467..114..
-// 1 ...*......
-// 2 ..35..633.
-// 3 ......#...
-// 4 617*......
-// 5 .....+.58.
-// 6 ..592.....
-// 7 ......755.
-// 8 ...$.*....
-// 9 .664.598..
-// yx0123456789
-test "adjacency tests" {
-    const symbols = [_]Symbol{
-        Symbol.init('*', 3, 1),
-        Symbol.init('#', 6, 3),
-        Symbol.init('*', 3, 4),
-        Symbol.init('+', 5, 5),
-        Symbol.init('$', 3, 8),
-        Symbol.init('*', 5, 8),
-    };
-    const ppns = [_]PossiblePartNumber{
-        PossiblePartNumber.init("467", 0, 0),
-        PossiblePartNumber.init("114", 5, 0),
-        PossiblePartNumber.init("35", 2, 2),
-        PossiblePartNumber.init("633", 6, 2),
-        PossiblePartNumber.init("617", 0, 4),
-        PossiblePartNumber.init("58", 7, 5),
-        PossiblePartNumber.init("592", 2, 6),
-        PossiblePartNumber.init("755", 6, 7),
-        PossiblePartNumber.init("664", 1, 9),
-        PossiblePartNumber.init("598", 5, 9),
-    };
-    // 467 is a part number (adjacent to the 1st *)
-    try _adjacency_tests(ppns[0], symbols[0], symbols[0..]);
-    // 114 is not a part number
-    for (symbols) |symbol| {
-        try expect(!ppns[1].is_adjacent_to_symbol(&symbol));
-    }
-    // 35 is a part number (adjacent to the 1st *)
-    try _adjacency_tests(ppns[2], symbols[0], symbols[0..]);
-    // 633 is a part number (adjacent to the #)
-    try _adjacency_tests(ppns[3], symbols[1], symbols[0..]);
-    // 617 is a part number (adjacent to the 2nd *)
-    try _adjacency_tests(ppns[4], symbols[2], symbols[0..]);
-    // 58 is not a part number
-    for (symbols) |symbol| try expect(!ppns[5].is_adjacent_to_symbol(&symbol));
-    // 592 is a part number (adjacent to the 1st +)
-    try _adjacency_tests(ppns[6], symbols[3], symbols[0..]);
-    // 755 is a part number (adjacent to the 3rd *)
-    try _adjacency_tests(ppns[7], symbols[5], symbols[0..]);
-    // 664 is a part number (adjacent to the 1st $)
-    try _adjacency_tests(ppns[8], symbols[4], symbols[0..]);
-    // 598 is a part number (adjacent to the 3rd *)
-    try _adjacency_tests(ppns[9], symbols[5], symbols[0..]);
-}
-
 const Symbol = struct {
     x: u16,
     y: u16,
@@ -187,68 +119,43 @@ pub fn main() !u8 {
         y += 1;
     }
 
+    var parts = std.ArrayList(PossiblePartNumber).init(gpa.allocator());
+    defer parts.deinit();
+
     var sum_of_part_numbers: usize = 0;
     for (possible_parts.items) |ppn| {
-        std.debug.print("{d},{d}: {d}\n", .{ ppn.y, ppn.x, ppn.value });
         for (symbols.items) |symbol| {
-            // std.debug.print("\t{d},{d}: {u}\n", .{ symbol.y, symbol.x, symbol.value });
             if (ppn.is_adjacent_to_symbol(&symbol)) {
-                // std.debug.print("\t\tFound a part number. {d} = {d} + {d}\n", .{
-                //     sum_of_part_numbers + ppn.value,
-                //     sum_of_part_numbers,
-                //     ppn.value,
-                // });
+                try parts.append(ppn);
                 sum_of_part_numbers += ppn.value;
                 break;
             }
         }
     }
 
+    var sum_of_gear_ratios: usize = 0;
+    for (symbols.items) |symbol| {
+        if (symbol.value != '*') continue;
+        var number_of_adjacent_parts: u8 = 0;
+        var gear_ratio: usize = 1;
+        for (parts.items) |pn| {
+            if (pn.is_adjacent_to_symbol(&symbol)) {
+                number_of_adjacent_parts += 1;
+                gear_ratio *= pn.value;
+            }
+        }
+        if (number_of_adjacent_parts == 2) {
+            sum_of_gear_ratios += gear_ratio;
+        }
+    }
+
+    // Note: for some reason that I will not search for at this time, the
+    // sum_of_part_numbers is different if run in powershell or in cmd:
+    // cat input.txt | day03.exe (ps) gives me 541515.
+    // day03.exe (cmd) gives me the correct number (540131).
+    // Both give the correct sum_of_gear_ratios.
     try stdout.print("Sum of part numbers is {d}\n", .{sum_of_part_numbers});
+    try stdout.print("Sum of gear ratios is {d}\n", .{sum_of_gear_ratios});
 
     return 0;
-}
-
-test "digit recognition" {
-    try expect(!is_digit('.'));
-    try expect(is_digit('0'));
-    try expect(is_digit('1'));
-    try expect(is_digit('2'));
-    try expect(is_digit('3'));
-    try expect(is_digit('4'));
-    try expect(is_digit('5'));
-    try expect(is_digit('6'));
-    try expect(is_digit('7'));
-    try expect(is_digit('8'));
-    try expect(is_digit('9'));
-    try expect(!is_digit('*'));
-    try expect(!is_digit('='));
-    try expect(!is_digit('-'));
-    try expect(!is_digit('/'));
-    try expect(!is_digit('%'));
-    try expect(!is_digit('$'));
-    try expect(!is_digit('&'));
-    try expect(!is_digit('@'));
-}
-
-test "symbol recognition" {
-    try expect(!is_symbol('.'));
-    try expect(!is_symbol('0'));
-    try expect(!is_symbol('1'));
-    try expect(!is_symbol('2'));
-    try expect(!is_symbol('3'));
-    try expect(!is_symbol('4'));
-    try expect(!is_symbol('5'));
-    try expect(!is_symbol('6'));
-    try expect(!is_symbol('7'));
-    try expect(!is_symbol('8'));
-    try expect(!is_symbol('9'));
-    try expect(is_symbol('*'));
-    try expect(is_symbol('='));
-    try expect(is_symbol('-'));
-    try expect(is_symbol('/'));
-    try expect(is_symbol('%'));
-    try expect(is_symbol('$'));
-    try expect(is_symbol('&'));
-    try expect(is_symbol('@'));
 }
